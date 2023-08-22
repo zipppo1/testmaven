2cafee22fdc09a0de8d0fe542cd17ac77f1e3d6e
zippo <118089868+zipppo1@users.noreply.github.com>
2023-08-22 17:20:02 +0900

    Add files via upload

---
+++ testinstance1/src/main/java/com/example/testinstance1/Testinstance1Application.java
@@ -0,0 +1,13 @@
+package com.example.testinstance1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testinstance1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testinstance1Application.class, args);
+	}
+
+}
---
+++ testinstance1/src/test/java/com/example/testinstance1/Testinstance1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testinstance1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testinstance1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
